c87283d32d6f939bb6568e01b87b9e6d7c440b55
Shalini Senthil <shaalsm@gmail.com>
2024-03-12 23:32:35 +0000

    added file text.py

---
+++ text.py
@@ -0,0 +1,20 @@
+""" 
+This program converts the adjective in the sentence to a verb. 
+
+The function has 2 paramaters - the sentence and index. 
+sentence: The input sentence with the adjective.
+index: The index of the adjective in the sentence. 
+
+The function returns the sentence with adjective replaced by a verb at the specified index. 
+The 'en' suffix is added to adjective to convert it into a verb. 
+Prints the new sentence with the verb. 
+"""
+
+def adjective_to_verb(sentence, index): 
+    new_word = ""
+    for word_ in sentence.split(" ")[index]: 
+        if word_.isalpha(): 
+            new_word += word_
+    return new_word + "en"
+print(adjective_to_verb('I need to make that bright.', -1))
+print(adjective_to_verb('It got dark as the sun set.', 2))
